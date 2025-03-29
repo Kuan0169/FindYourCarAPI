613113da1a4ebc8d449177fb27d4ed047c01e1ad
Kuan <h0169888@gmail.com>
2025-03-29 13:23:02 +0100

    update controllers

--- FindYourCarAPI.Services/Services/CarService.cs
+++ FindYourCarAPI.Services/Services/CarService.cs
@@ -9,22 +9,41 @@ namespace FindYourCarAPI.Services.Services
         private readonly TestContext context;
 
         public CarService(TestContext context)
-        { 
+        {
             this.context = context;
         }
 
-        public async Task<IEnumerable<Car>> GetAll() 
+        public async Task<IEnumerable<Car>> GetAll()
         {
             return await context.Cars.ToListAsync();
         }
 
-        public async Task<Car> GetrById(int id) 
+        public async Task<Car> GetrById(int id)
         {
             return await context.Cars.FindAsync(id);
-        } 
+        }
         public async Task<Car> Create(Car car)
         {
             context.Cars.Add(car);
+            await context.SaveChangesAsync();
+            return car;
+        }
+
+        public async Task<Car> Update(Car car)
+        {
+            context.Cars.Update(car);
+            await context.SaveChangesAsync();
+            return car;
+        }
+
+        public async Task<Car> Delete(int id)
+        {
+            var car = await context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return null;
+            }
+            context.Cars.Remove(car);
             await context.SaveChangesAsync();
             return car;
         }
@@ -33,7 +52,10 @@ namespace FindYourCarAPI.Services.Services
     public interface ICarService
     {
         Task<IEnumerable<Car>> GetAll();
-        Task<Car> Create(Car car);  
-        Task<Car> GetrById( int id );
+        Task<Car> Create(Car car);
+        Task<Car> GetrById(int id);
+        Task<Car> Update(Car car);
+        Task<Car> Delete(int id);
+    
     }
 }
--- FindYourCarAPI/Controllers/TestController.cs
+++ FindYourCarAPI/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-﻿using FindYourCarAPI.Services.Services;
+﻿using FindYourCarAPI.Data.Entities;
+using FindYourCarAPI.Services.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
 
 
 namespace FindYourCarAPI.Controllers
@@ -21,5 +23,35 @@ namespace FindYourCarAPI.Controllers
             var cars = await carService.GetAll();
             return Ok(cars);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var car = await carService.GetrById(id);
+            return car != null ? Ok(car) : NotFound("Car not found. Go get one!!!");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Car car)
+        {
+            var createCar = await carService.Create(car);
+
+            return Ok(createCar);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Car car)
+        {
+            var updatedCar = await carService.Update(car);
+            return Ok(updatedCar);
+        }
+
+        [HttpDelete("{id}")]  
+        public async Task<IActionResult> Delete(int id)
+        {
+            var reslt = await carService.Delete(id);
+            return Ok(reslt);
+        
+        } 
     }
 }
